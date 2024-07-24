b318f1f93e637f51b9ec5c410937ea6e9a9c9815
Burak Benligiray <bbenligiray@gmail.com>
2024-07-24 21:42:12 +0300

    Update rules

--- rules.js
+++ rules.js
@@ -1,9 +1,10 @@
 module.exports = {
     rules: `
 - No scams
-- No spam, self-promotion or advertising
+- No spam or advertising
 - No partnership offers
 - No posts seeking employment
 - No excessive profanity
+- Chain, data feed, dApp integration requests are allowed
 `
 };
