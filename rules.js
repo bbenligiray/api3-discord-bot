1c1c239c7cfc493f89439aaf02474961151d3460
metobom <unutkan2013@yandex.com>
2024-07-26 13:31:22 +0300

    Format

--- rules.js
+++ rules.js
@@ -1,10 +1,10 @@
 module.exports = {
-    rules: `
+  rules: `
 - No scams
 - No spam or advertising
 - No partnership offers
 - No posts seeking employment
 - No excessive profanity
 - Chain, data feed, dApp integration requests are allowed
-`
+`,
 };
